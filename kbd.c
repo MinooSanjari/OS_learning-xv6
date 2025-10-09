da26b0643cf464c678066e0291770d4f4680d7cf
FaezehM05 <misaghifaezeh@gmail.com>
2025-10-09 22:30:58 +0900

    Add all code including OS_learning-xv6

---
+++ kbd.c
@@ -0,0 +1,59 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "kbd.h"
+
+int
+kbdgetc(void)
+{
+  static uint shift;
+  static uchar *charcode[4] = {
+    normalmap, shiftmap, ctlmap, ctlmap
+  };
+  uint st, data, c;
+
+  st = inb(KBSTATP);
+  if((st & KBS_DIB) == 0)
+    return -1;
+  data = inb(KBDATAP);
+
+  if(data == 0xE0){
+    shift |= E0ESC;
+    return 0;
+  } else if(data & 0x80){
+    // Key released
+    data = (shift & E0ESC ? data : data & 0x7F);
+    shift &= ~(shiftcode[data] | E0ESC);
+    return 0;
+  } else if(shift & E0ESC){
+    // Last character was an E0 escape; or with 0x80
+    data |= 0x80;
+    shift &= ~E0ESC;
+  }
+
+  shift |= shiftcode[data];
+  shift ^= togglecode[data];
+  c = charcode[shift & (CTL | SHIFT)][data];
+  if(shift & CAPSLOCK){
+    if('a' <= c && c <= 'z')
+      c += 'A' - 'a';
+    else if('A' <= c && c <= 'Z')
+      c += 'a' - 'A';
+  }
+  if(c == 0){
+    switch(data){
+    case 0xCB: return KEY_LF;  // left arrow
+    case 0xCD: return KEY_RT;  // right arrow
+    case 0xC8: return KEY_UP;
+    case 0xD0: return KEY_DN;
+    }
+  }
+
+  return c;
+}
+
+void
+kbdintr(void)
+{
+  consoleintr(kbdgetc);
+}
